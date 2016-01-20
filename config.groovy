13f214a1e99c8b9e0998ff9de77a3a9a56716998
Koji Kuwana <koji.kuwana@gmail.com>
2016-01-20 12:39:00 +0100

    Initial

---
+++ config.groovy
@@ -0,0 +1,12 @@
+// The title of this document
+//title = "Gaiden"
+title = "Grailsチュートリアル"
+
+// The version of this document
+//version = ""
+
+// The path of source files directory
+//sourceDirectoryPath = "."
+
+// The path of directory to be outputted a document
+//outputDirectoryPath = "build"
